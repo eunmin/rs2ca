b66f2f0a45bc63a2bdef5d9d6d7373bf55a49fd5
Eunmin Kim <telepopsound@gmail.com>
2020-05-19 22:57:21 +0900

    Add some examples

--- src/rs2ca/core.clj
+++ src/rs2ca/core.clj
@@ -2,7 +2,12 @@
   (:require [clojure.core.async :refer [chan <!! put! close! >!! go <! >! buffer]])
   (:import [java.util.function BiFunction]
            [io.r2dbc.spi ConnectionFactories ConnectionFactoryOptions]
-           [org.reactivestreams Publisher Subscriber Subscription]))
+           [org.reactivestreams Publisher Subscriber Subscription]
+           [com.mongodb.reactivestreams.client
+            MongoClient
+            MongoClients
+            MongoCollection
+            MongoDatabase]))
 
 (defn pub->chan [p]
   (let [c (chan (buffer 1))
@@ -12,7 +17,6 @@
                       (reset! subscription s)
                       (.request @subscription 1))
                     (onNext [o]
-;;                      (println "next:" o "thread:" (.getName (Thread/currentThread)))
                       (put! c o)
                       (.request @subscription 1))
                     (onError [t]
@@ -21,4 +25,35 @@
                       (close! c))))
     c))
 
+;; Async postgresql client example
 
+(def options
+  (let [bd (ConnectionFactoryOptions/builder)]
+    (.option bd ConnectionFactoryOptions/DRIVER "postgresql")
+    (.option bd ConnectionFactoryOptions/HOST "localhost")
+    (.option bd ConnectionFactoryOptions/PORT (int 5432))
+    (.option bd ConnectionFactoryOptions/DATABASE "test")
+    (.build bd)))
+
+(def factory (ConnectionFactories/get options))
+
+(defn run []
+  (go
+    (let [conn (<! (pub->chan (.create factory)))
+          result (<! (pub->chan (.execute (.createStatement conn "select * from users"))))
+          rows (pub->chan (.map result (proxy [BiFunction] []
+                                         (apply [row meta]
+                                           (.get row "name")))))]
+      (println (<! rows))
+      (println (<! rows)))))
+
+;; Async Mongoclient example
+
+(def client (MongoClients/create))
+(def db (.getDatabase client "test"))
+(def coll (.getCollection db "users"))
+
+(defn run2 []
+  (go
+    (let [doc (<! (pub->chan (.find coll)))]
+      (println doc))))
